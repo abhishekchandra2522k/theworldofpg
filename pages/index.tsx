8690b8f7f5a6d30ffb5dae3a293dbd7cf7823cfb
abhishekchandra2522k <1ac23456789@gmail.com>
2022-11-23 13:10:59 +0530

    Initial commit, add code setup files for next.js, tailwindcss, and graphql

---
+++ pages/index.tsx
@@ -0,0 +1,43 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { PostCard, Categories, PostWidget } from '../components'
+
+const posts = [
+  {
+    title: 'Feeling Thankful Is Helpful for Anxiety',
+    excerpt: 'Learn about managing anxiety',
+  },
+  {
+    title: 'Staying Organized Helps My Anxiety',
+    excerpt: 'Learn about managing anxiety',
+  },
+]
+
+const Home: NextPage = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <Head>
+        <title>The World Of PG</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12 text-white'>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
