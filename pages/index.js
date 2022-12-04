5bd8f8cc0269f2e8ee027168ec4a7bd833e63ad5
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-05 00:00:36 +0530

    Replace index.tsx to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
 import { FeaturedPosts } from '../sections'
