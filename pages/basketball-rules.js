0e13485511d2400826b421d484e98bbddcb3ab0b
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-14 19:11:32 +0530

    Add basketball-rules.js page

---
+++ pages/basketball-rules.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BasketBallRules } from '../components'
+
+const BasketBallRule = () => {
+  return (
+    <div className='pt-16'>
+      <BasketBallRules />
+    </div>
+  )
+}
+export default BasketBallRule
