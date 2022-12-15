a499380d94b0ff6f598d96d7a80ecc044aced41f
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-15 17:07:42 +0530

    Adjust grid gap

--- sections/AdjacentPosts.jsx
+++ sections/AdjacentPosts.jsx
@@ -15,7 +15,7 @@ const AdjacentPosts = ({ createdAt, slug }) => {
   }, [slug])
 
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-8 gap-12 mb-8'>
+    <div className='grid grid-cols-1 lg:grid-cols-8 gap-10 mb-8'>
       {dataLoaded && (
         <>
           {adjacentPost.previous && (
