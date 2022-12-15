00db0e5634177dbaeb7934997076f0219aa1ee40
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-15 17:05:31 +0530

    Add Footer to the layout

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='flex flex-col items-center text-white p-2 w-full bg-black bg-opacity-60'>
+        <span>© 2023 The World of PG. All rights reserved.</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
