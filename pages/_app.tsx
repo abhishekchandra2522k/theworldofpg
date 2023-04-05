9778d29056a97cf8475ee51af322c1def5f39c73
abhishekchandra2522k <1ac23456789@gmail.com>
2023-04-05 21:03:07 +0530

    Add Audiences analytics to pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { Layout } from '../components'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
